b1fb859ae1cf628dd3cc13b673ec57d29560acb1
Dheeraj Sharma <dextersharma960@gmail.com>
2020-01-27 21:15:23 +0530

    Alerts and Messages

---
+++ frontend/src/components/layout/Alerts.js
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from "react";
+import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import propTypes from "prop-types";
+
+export class Alerts extends Component {
+  static propTypes = {
+    error: propTypes.object.isRequired,
+    message: propTypes.object.isRequired
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error, alert, message } = this.props;
+    if (error !== prevProps.error) {
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+      if (error.msg.message)
+        alert.error(`Message: ${error.msg.message.join()}`);
+    }
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+    }
+    if (message !== prevProps.message) {
+      if (message.addLead) alert.success(message.addLead);
+    }
+  }
+
+  render() {
+    return <Fragment />;
+  }
+}
+
+const mapStateToProps = state => ({
+  error: state.errors,
+  message: state.messages
+});
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from "redux";
 import leads from "./leads";
+import errors from "./errors";
+import messages from "./messages";
 export default combineReducers({
-  leads
+  leads,
+  errors,
+  messages
 });
